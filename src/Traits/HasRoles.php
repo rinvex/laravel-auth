849458ac3dec800effb4632862e52cd1544e6ec2
Abdelrahman Omran <me@omranic.com>
2016-12-23 23:35:44 +0200

    Refactor roles assignment and checking

--- src/Traits/HasRoles.php
+++ src/Traits/HasRoles.php
@@ -23,85 +23,72 @@ trait HasRoles
     use HasAbilities;
 
     /**
-     * Assign the given role(s) to the entity.
+     * Attach the given role(s) to the model.
      *
-     * @param mixed $roles
+     * @param int|string|array|\ArrayAccess|\Rinvex\Fort\Models\Role $roles
      *
      * @return $this
      */
     public function assignRoles($roles)
     {
-        // Array of role slugs
-        $roles = $this->hydrateIfString($roles);
-
-        // Single role model
-        if ($roles instanceof Role) {
-            $roles = [$roles->id];
-        }
-
-        // Fire the role assigning event
-        static::$dispatcher->fire('rinvex.fort.role.assigning', [$this, $roles]);
-
-        // Assign roles
-        $this->roles()->syncWithoutDetaching($roles);
-
-        // Fire the role assigned event
-        static::$dispatcher->fire('rinvex.fort.role.assigned', [$this, $roles]);
+        static::setRoles('syncWithoutDetaching', $roles);
 
         return $this;
     }
 
     /**
-     * Sync the given role(s) to the entity.
+     * Sync the given role(s) to the model.
      *
-     * @param mixed $roles
+     * @param int|string|array|\ArrayAccess|\Rinvex\Fort\Models\Role $roles
      *
      * @return $this
      */
     public function syncRoles($roles)
     {
-        // Array of role slugs
-        $roles = $this->hydrateIfString($roles);
-
-        // Single role model
-        if ($roles instanceof Role) {
-            $roles = [$roles->id];
-        }
-
-        // Fire the role syncing event
-        static::$dispatcher->fire('rinvex.fort.role.syncing', [$this, $roles]);
-
-        // Assign roles
-        $this->roles()->sync($roles);
-
-        // Fire the role synced event
-        static::$dispatcher->fire('rinvex.fort.role.synced', [$this, $roles]);
+        static::setRoles('sync', $roles);
 
         return $this;
     }
 
     /**
-     * Remove the given role(s) from the entity.
+     * Detach the given role(s) from the model.
      *
-     * @param mixed $roles
+     * @param int|string|array|\ArrayAccess|\Rinvex\Fort\Models\Role $roles
      *
      * @return $this
      */
     public function removeRoles($roles)
     {
-        // Array of role slugs
-        $roles = $this->hydrateIfString($roles);
-
-        // Fire the role removed event
-        static::$dispatcher->fire('rinvex.fort.role.removing', [$this, $roles]);
-
-        // Detach roles
-        $this->roles()->detach($roles);
-
-        // Fire the role removed event
-        static::$dispatcher->fire('rinvex.fort.role.removed', [$this, $roles]);
+        static::setRoles('detach', $roles);
 
         return $this;
+    }
+
+    /**
+     * Set the given role(s) to the model.
+     *
+     * @param int|string|array|\ArrayAccess|\Rinvex\Fort\Models\Role $roles
+     * @param string                                                 $action
+     *
+     * @return void
+     */
+    protected function setRoles($roles, string $action)
+    {
+        // Fix exceptional event name
+        $event = $action == 'syncWithoutDetaching' ? 'attach' : $action;
+
+        // Hydrate Roles
+        $roles = static::hydrateRoles($roles)->pluck('id')->toArray();
+
+
+        // Fire the role syncing event
+        static::$dispatcher->fire("rinvex.fort.role.{$event}ing", [$this, $roles]);
+
+        // Set roles
+        $this->roles()->$action($roles);
+
+        // Fire the role synced event
+        static::$dispatcher->fire("rinvex.fort.role.{$event}ed", [$this, $roles]);
     }
 
     /**
@@ -118,19 +105,24 @@ trait HasRoles
             return $this->roles->contains('slug', $roles);
         }
 
+        // Single role id
+        if (is_string($roles)) {
+            return $this->roles->contains('id', $roles);
+        }
+
         // Single role model
         if ($roles instanceof Role) {
             return $this->roles->contains('slug', $roles->slug);
         }
 
         // Array of role slugs
-        if (is_array($roles) && is_string($roles[0])) {
-            return $this->roles->pluck('slug')->intersect($roles)->isEmpty();
+        if (is_array($roles) && isset($roles[0]) && is_string($roles[0])) {
+            return ! $this->roles->pluck('slug')->intersect($roles)->isEmpty();
         }
 
         // Array of role Ids
-        if (is_array($roles) && is_int($roles[0])) {
-            return $this->roles->pluck('id')->intersect($roles)->isEmpty();
+        if (is_array($roles) && isset($roles[0]) && is_int($roles[0])) {
+            return ! $this->roles->pluck('id')->intersect($roles)->isEmpty();
         }
 
         // Collection of role models
@@ -167,31 +159,73 @@ trait HasRoles
             return $this->roles->contains('slug', $roles);
         }
 
+        // Single role id
+        if (is_int($roles)) {
+            return $this->roles->contains('id', $roles);
+        }
+
         // Single role model
         if ($roles instanceof Role) {
             return $this->roles->contains('slug', $roles->slug);
         }
 
-        // Array of role slugs OR Collection of role models
-        if ($roles instanceof Collection || (is_array($roles) && is_string($roles[0]))) {
+        // Array of role slugs
+        if (is_array($roles) && isset($roles[0]) && is_string($roles[0])) {
             return $this->roles->pluck('slug')->count() == count($roles)
                    && $this->roles->pluck('slug')->diff($roles)->isEmpty();
         }
 
-        return $this->roles->pluck('id')->count() == count($roles)
-               && $this->roles->pluck('id')->diff($roles)->isEmpty();
+        // Array of role ids
+        if (is_array($roles) && isset($roles[0]) && is_int($roles[0])) {
+            return $this->roles->pluck('id')->count() == count($roles)
+                   && $this->roles->pluck('id')->diff($roles)->isEmpty();
+        }
+
+        // Collection of role models
+        if ($roles instanceof Collection) {
+            return $this->roles->count() == $roles->count() && $this->roles->diff($roles)->isEmpty();
+        }
+
+        return false;
     }
 
     /**
-     * Hydrate roles if it's string based.
+     * Hydrate roles.
      *
-     * @param $roles
+     * @param int|string|array|\ArrayAccess|\Rinvex\Fort\Models\Role $roles
      *
-     * @return array
+     * @return \Illuminate\Support\Collection
      */
-    protected function hydrateIfString($roles)
+    protected function hydrateRoles($roles)
     {
-        return is_string($roles) || (is_array($roles) && is_string($roles[0]))
-            ? Role::whereIn('slug', (array) $roles)->get() : $roles;
+        $isRolesStringBased = static::isRolesStringBased($roles);
+        $isRolesIntBased = static::isRolesIntBased($roles);
+        $field = $isRolesStringBased ? 'slug' : 'id';
+
+        return $isRolesStringBased || $isRolesIntBased ? Role::whereIn($field, (array) $roles)->get() : collect($roles);
+    }
+
+    /**
+     * Determine if the given role(ies) are string based.
+     *
+     * @param int|string|array|\ArrayAccess|\Rinvex\Fort\Models\Role $roles
+     *
+     * @return bool
+     */
+    protected function isRolesStringBased($roles)
+    {
+        return is_string($roles) || (is_array($roles) && isset($roles[0]) && is_string($roles[0]));
+    }
+
+    /**
+     * Determine if the given role(ies) are integer based.
+     *
+     * @param int|string|array|\ArrayAccess|\Rinvex\Fort\Models\Role $roles
+     *
+     * @return bool
+     */
+    protected function isRolesIntBased($roles)
+    {
+        return is_int($roles) || (is_array($roles) && isset($roles[0]) && is_int($roles[0]));
     }
 }
