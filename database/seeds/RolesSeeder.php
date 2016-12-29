bb9bf11ba0c92d252950fbe6e1d67b08a762b992
Marcus Reinhardt <webstone@gmail.com>
2016-12-29 16:32:21 +0100

    Updated Roles Seed

--- database/seeds/RolesSeeder.php
+++ database/seeds/RolesSeeder.php
@@ -20,6 +20,9 @@ use Illuminate\Support\Facades\DB;
 
 class RolesSeeder extends Seeder
 {
+    protected $before   = "before_";
+	protected $after    = "after_";
+    
     /**
      * Run the database seeds.
      *
@@ -27,8 +30,13 @@ class RolesSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table(config('rinvex.fort.tables.roles'))->truncate();
+        $connection = config('database.default');
+		
+		$this->command->info('Truncating table: '.config('rinvex.fort.tables.roles'));
+		
+		$this->{$this->before.$connection}();
+		
+		$this->command->info('Creating default roles');
 
         // Get roles data
         $roles = json_decode(file_get_contents(__DIR__.'/../../resources/data/roles.json'), true);
@@ -41,6 +49,65 @@ class RolesSeeder extends Seeder
         // Grant abilities to roles
         app('rinvex.fort.role')->findBy('slug', 'admin')->grantAbilityTo('superadmin', 'global');
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        $this->{$this->after.$connection}();
     }
+    
+    /**
+	 * Defines the post actions before the seed has been executed
+	 *
+	 * @return void
+	 */
+	protected function before_mysql() {
+		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+		DB::table(config('rinvex.fort.tables.roles'))->truncate();
+	}
+	
+	/**
+	 * Defines the post actions before the seed has been executed
+	 *
+	 * @return void
+	 */
+	protected function before_sqlite() {
+		DB::table(config('rinvex.fort.tables.roles'))->truncate();
+	}
+	
+	/**
+	 * Defines the post actions before the seed has been executed
+	 *
+	 * @return void
+	 */
+	protected function before_pgsql() {
+		$tables = [
+			config('rinvex.fort.tables.roles')
+		];
+		
+		DB::statement('TRUNCATE TABLE ' . implode(',', $tables). ' CASCADE;');
+	}
+	
+	/**
+	 * Defines the actions after the seed has been executed
+	 *
+	 * @return void
+	 */
+	protected function after_mysql() {
+		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+	}
+	
+	/**
+	 * Defines the actions after the seed has been executed
+	 *
+	 * @return void
+	 */
+	protected function after_sqlite() {
+		//nothing to do here
+	}
+	
+	/**
+	 * Defines the actions after the seed has been executed
+	 *
+	 * @return void
+	 */
+	protected function after_pgsql() {
+		//nothing to do here
+	}
 }
