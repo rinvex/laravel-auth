39201ec5e2e7dff84de2b9f74462863666aa6a39
Abdelrahman Omran <me@omranic.com>
2017-02-05 20:15:35 +0200

    Override ability unique validation rule to replace nulls with ignored values

--- src/Models/Ability.php
+++ src/Models/Ability.php
@@ -242,4 +242,51 @@ class Ability extends Model
     {
         return $this->action.'-'.$this->resource;
     }
+
+    /**
+     * Prepare a unique rule, adding the table name, column and model indetifier
+     * if required.
+     *
+     * @param  array  $parameters
+     * @param  string $field
+     *
+     * @return string
+     */
+    protected function prepareUniqueRule($parameters, $field)
+    {
+        // If the table name isn't set, infer it.
+        if (empty($parameters[0])) {
+            $parameters[0] = $this->getModel()->getTable();
+        }
+
+        // If the connection name isn't set but exists, infer it.
+        if ((strpos($parameters[0], '.') === false) && (($connectionName = $this->getModel()->getConnectionName()) !== null)) {
+            $parameters[0] = $connectionName.'.'.$parameters[0];
+        }
+
+        // If the field name isn't get, infer it.
+        if (! isset($parameters[1])) {
+            $parameters[1] = $field;
+        }
+
+        if ($this->exists) {
+            // If the identifier isn't set, infer it.
+            if (! isset($parameters[2]) || strtolower($parameters[2]) === 'null') {
+                $parameters[2] = $this->getModel()->getKey();
+            }
+
+            // If the primary key isn't set, infer it.
+            if (! isset($parameters[3])) {
+                $parameters[3] = $this->getModel()->getKeyName();
+            }
+
+            foreach ($parameters as $key => $parameter) {
+                if (strtolower($parameter) === 'null') {
+                    $parameters[$key] = $this->getModel()->{$parameters[$key-1]};
+                }
+            }
+        }
+
+        return 'unique:'.implode(',', $parameters);
+    }
 }
