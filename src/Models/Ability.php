22fc18163b7cb20ca569160019820552b5323826
Abdelrahman Omran <me@omranic.com>
2017-06-16 16:25:34 +0200

    Fix validation rule nullability issue

--- src/Models/Ability.php
+++ src/Models/Ability.php
@@ -118,8 +118,8 @@ class Ability extends Model
         $this->setTable(config('rinvex.fort.tables.abilities'));
         $this->setRules([
             'name' => 'required|string|max:250',
-            'action' => 'required|unique:'.config('rinvex.fort.tables.abilities').',action,NULL,id,resource,'.$this->resource,
-            'resource' => 'required|unique:'.config('rinvex.fort.tables.abilities').',resource,NULL,id,action,'.$this->action,
+            'action' => 'required|unique:'.config('rinvex.fort.tables.abilities').',action,NULL,id,resource,'.($this->resource ?? 'null'),
+            'resource' => 'required|unique:'.config('rinvex.fort.tables.abilities').',resource,NULL,id,action,'.($this->action ?? 'null'),
         ]);
     }
 
