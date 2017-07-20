6cca4734aa5f5b3586a1b08e1f5c1466d0d9f791
Abdelrahman Omran <me@omranic.com>
2017-07-20 16:08:57 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/UpdateLastActivity.php
+++ src/Http/Middleware/UpdateLastActivity.php
@@ -37,7 +37,7 @@ class UpdateLastActivity
             // We are using database queries rather than eloquent, to bypass triggering events.
             // Triggering update events flush cache and costs us more queries, which we don't need.
             $userModel = config('auth.providers.'.config('auth.guards.'.config('auth.defaults.guard').'.provider').'.model');
-            (new $userModel)->where('id', $user->id)->update(['last_activity' => new Carbon()]);
+            (new $userModel())->where('id', $user->id)->update(['last_activity' => new Carbon()]);
         }
     }
 }
