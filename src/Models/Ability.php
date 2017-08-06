660d2b25e0c24d2f4f3b7cea46f00e1094d125b7
Abdelrahman Omran <me@omranic.com>
2017-08-07 01:25:56 +0200

    Move prepareUniqueRule validation method to it's own support trait

--- src/Models/Ability.php
+++ src/Models/Ability.php
@@ -8,6 +8,7 @@ use Watson\Validating\ValidatingTrait;
 use Illuminate\Database\Eloquent\Model;
 use Rinvex\Cacheable\CacheableEloquent;
 use Spatie\Translatable\HasTranslations;
+use Rinvex\Support\Traits\UniqueInjector;
 
 /**
  * Rinvex\Fort\Models\Ability.
@@ -39,8 +40,11 @@ use Spatie\Translatable\HasTranslations;
 class Ability extends Model
 {
     use HasTranslations;
-    use ValidatingTrait;
     use CacheableEloquent;
+    use ValidatingTrait, UniqueInjector
+    {
+        UniqueInjector::prepareUniqueRule insteadof ValidatingTrait;
+    }
 
     /**
      * {@inheritdoc}
@@ -306,55 +310,6 @@ class Ability extends Model
     public function getSlugAttribute()
     {
         return $this->action.'-'.$this->resource;
-    }
-
-    /**
-     * Prepare a unique rule, adding the table name, column and model indetifier
-     * if required.
-     *
-     * @param array  $parameters
-     * @param string $field
-     *
-     * @return string
-     */
-    protected function prepareUniqueRule($parameters, $field)
-    {
-        // If the table name isn't set, infer it.
-        if (empty($parameters[0])) {
-            $parameters[0] = $this->getModel()->getTable();
-        }
-
-        // If the connection name isn't set but exists, infer it.
-        if ((mb_strpos($parameters[0], '.') === false) && (($connectionName = $this->getModel()->getConnectionName()) !== null)) {
-            $parameters[0] = $connectionName.'.'.$parameters[0];
-        }
-
-        // If the field name isn't get, infer it.
-        if (! isset($parameters[1])) {
-            $parameters[1] = $field;
-        }
-
-        if ($this->exists) {
-            // If the identifier isn't set, infer it.
-            if (! isset($parameters[2]) || mb_strtolower($parameters[2]) === 'null') {
-                $parameters[2] = $this->getModel()->getKey();
-            }
-
-            // If the primary key isn't set, infer it.
-            if (! isset($parameters[3])) {
-                $parameters[3] = $this->getModel()->getKeyName();
-            }
-
-            // If the additional where clause isn't set, infer it.
-            // Example: unique:abilities,resource,123,id,action,NULL
-            foreach ($parameters as $key => $parameter) {
-                if (mb_strtolower((string) $parameter) === 'null') {
-                    $parameters[$key] = $this->getModel()->{$parameters[$key - 1]};
-                }
-            }
-        }
-
-        return 'unique:'.implode(',', $parameters);
     }
 
     /**
