703d2b72e0f9c80cf0671cfe319bebb3c6ff359d
Abdelrahman Omran <me@omranic.com>
2017-08-27 19:37:22 +0200

    Trim Model name if ModelContract given

--- src/Handlers/ExceptionHandler.php
+++ src/Handlers/ExceptionHandler.php
@@ -24,8 +24,9 @@ class ExceptionHandler extends Handler
     public function render($request, Exception $exception)
     {
         if ($exception instanceof ModelNotFoundException) {
+            $model = str_replace('Contract', '', $exception->getModel());
             $isBackend = mb_strpos($request->route()->getName(), 'backend') !== false;
-            $single = mb_strtolower(mb_substr($exception->getModel(), mb_strrpos($exception->getModel(), '\\') + 1));
+            $single = mb_strtolower(mb_substr($model, mb_strrpos($model, '\\') + 1));
             $plural = str_plural($single);
 
             return intend([
