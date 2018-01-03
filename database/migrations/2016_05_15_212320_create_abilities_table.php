da1efa9497e50cbf361fbd84d1b33b8b4503715d
Abdelrahman Omran <me@omranic.com>
2018-01-03 13:00:30 +0200

    Fix abilities name/description has wrong field data type -> json

--- database/migrations/2016_05_15_212320_create_abilities_table.php
+++ database/migrations/2016_05_15_212320_create_abilities_table.php
@@ -21,8 +21,8 @@ class CreateAbilitiesTable extends Migration
             $table->string('action');
             $table->string('resource');
             $table->string('policy')->nullable();
-            $table->string('name');
-            $table->text('description')->nullable();
+            $table->{$this->jsonable()}('name');
+            $table->{$this->jsonable()}('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
@@ -39,5 +39,17 @@ class CreateAbilitiesTable extends Migration
     public function down()
     {
         Schema::dropIfExists(config('rinvex.fort.tables.abilities'));
+    }
+
+    /**
+     * Get jsonable column data type.
+     *
+     * @return string
+     */
+    protected function jsonable()
+    {
+        return DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) === 'mysql'
+               && version_compare(DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION), '5.7.8', 'ge')
+            ? 'json' : 'text';
     }
 }
