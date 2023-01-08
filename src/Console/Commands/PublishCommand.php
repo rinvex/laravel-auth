3fd9c7ddb521ebc8a999e2cdd35af0f22deba76e
Abdelrahman Omran <me@omranic.com>
2023-01-09 00:23:19 +0400

    Utilize PHP 8.1 attributes feature for artisan commands

--- src/Console/Commands/PublishCommand.php
+++ src/Console/Commands/PublishCommand.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Rinvex\Auth\Console\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: 'rinvex:publish:auth')]
 class PublishCommand extends Command
 {
     /**
